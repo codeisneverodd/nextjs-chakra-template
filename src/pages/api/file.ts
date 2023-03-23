07d2ed90276e66255d1d6b30ae5f80f69e83eec4
codeisneverodd <codeisneverodd@gmail.com>
2023-03-23 23:04:37 +0900

    feat: add file api mockup

---
+++ src/pages/api/file.ts
@@ -0,0 +1,28 @@
+import { promises as fs } from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+
+const jsonDirectory = path.join(process.cwd(), "data");
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResData>
+) {
+  if (req.method === "GET") {
+    try {
+      await fs.readFile(`${jsonDirectory}/solutions.json`, "utf-8");
+      await fs.writeFile(
+        `${jsonDirectory}/solutions.json`,
+        JSON.stringify({}),
+        {
+          encoding: "utf-8"
+        }
+      );
+
+      res.status(200).json([]);
+    } catch {
+      res.status(500).json([]);
+    }
+  }
+}
+export type ResData = {};
